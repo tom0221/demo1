6d8066f957e9ac493e6c3c667c17ff0a67938cf5
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-29 18:40:55 +0900

    [finish]topページ表示

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,8 @@ module Demo1
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  def after_sign_in_path_for(resource)
+  	images_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'homes#top'
+
+  resources :images, only: [:new, :create, :index, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
