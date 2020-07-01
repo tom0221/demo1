93386114cff09487b30b29c2811b9d47c6fcb4d3
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-01 11:30:48 +0900

    レイアウト色付け

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,26 @@
+class ImagesController < ApplicationController
+
+	def create
+
+	end
+
+	def index
+
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'imeges#index'
+  root 'homes#top'
 
-  resources :images, only: [:new, :create, :index, :edit, :update, :destroy]
+  get "/about" => "homes#about"
+
+  resources :images, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
