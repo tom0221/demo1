02365723615462a399222b589ffa26371e971765
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-02 12:01:23 +0900

    usersプロフィールカラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -10,5 +8,5 @@ Rails.application.routes.draw do
   get "/about" => "homes#about"
 
   resources :images, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
