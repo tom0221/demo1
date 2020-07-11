95b043734d668a7960329a5faa88b0ad162e688a
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-12 00:29:13 +0900

    Hello jQuery-test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
     // ナビゲーションの形("block" or "bubble")
     navType : 'block',
     // 子要素の種類("div" or "img")
-    childrenElementType : 'div',
+    childrenElementType : 'img',
     // ナビゲーション矢印の表示(trueで表示)
     arrows : true,
     // スライドショーの自動再生(falseで自動再生なし)
@@ -39,6 +39,12 @@ $(document).ready(function () {
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
     hidePrevious : false
+  });
+});
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
   });
 });
 
